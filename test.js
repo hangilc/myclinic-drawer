d2c8a3005e71c4858d4dd0f7533ad2e632a17f11
hangilc <hangil@chang.jp>
2016-08-17 09:47:18 +0900

    extended test.js

--- test.js
+++ test.js
@@ -5,6 +5,14 @@ var drawer = require("./index");
 var comp = new drawer.Compiler();
 comp.moveTo(10, 10);
 comp.lineTo(30, 20);
+comp.createFont("gothic", "MS Gothic", 10, 0, 0);
+comp.setFont("gothic");
+comp.textAt("Hello, world", 10, 20, "left", "top");
+comp.setTextColor(0, 0, 255);
+comp.textAt("Hello, world", 10, 40, "left", "top");
+comp.createPen("green", 0, 255, 0, 0.1);
+comp.setPen("green");
+comp.moveTo(10, 50);
+comp.lineTo(30, 60);
 var ops = comp.getOps();
 console.log(JSON.stringify([ops], null, 4));
-
