d5d1721689b67e9b12e481d60cbefb365b4eabd2
hangilc <hangil@chang.jp>
2016-08-21 22:00:45 +0900

    developing drawer-svg

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 var Ops = require("./drawer-ops");
 var Box = require("./drawer-box");
 var Compiler = require("./drawer-compiler");
+var SVG = require("./drawer-svg");
 
 exports.op = Ops;
 exports.Box = Box;
 exports.Compiler = Compiler;
+exports.drawerToSvg = SVG.drawerToSvg;
 
 
